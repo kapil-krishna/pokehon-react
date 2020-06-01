d3d5bc65fb890e59472fb22e4584ab3d118d745e
Kapil Krishna <kapil.krishna.yo@gmail.com>
2020-06-01 18:34:09 +0100

    First commit

---
+++ src/pages/about/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function About() {
+    return (
+        <div className="container" id="about-container">
+            <h1>About</h1>
+            <h2>This website was built using React.</h2>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Homepage() {
+    return (
+        <div className="container" id="welcome-container">
+            <h1>Welcome to PokeHON</h1>
+            <h2>The ultimate decider in who (or what) is the best looking pokemon of all time</h2>
+        </div>
+    );
+}
+
+export default Homepage;
